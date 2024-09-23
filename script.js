98284167be185b71ba3d0d56d2809b158a9284d2
sadiahimi <sadia.ahmed.himi@gmail.com>
2024-09-24 01:14:22 +0600

    blog added

--- script.js
+++ script.js
@@ -11,3 +11,27 @@ document.getElementById('historyButton').onclick = function() {
     document.getElementById('historySection').classList.remove('hidden');
     toggleActiveButton('historyButton');
 };
+document.querySelectorAll('.donate-button').forEach(button => {
+    button.onclick = function() {
+        const card = this.closest('.donation-card');
+        const inputField = card.querySelector('.donation-input');
+        const currentAmountSpan = card.querySelector('.current-amount');
+        const donationAmount = parseFloat(inputField.value);
+        
+        if (validateDonation(donationAmount)) {
+            accountBalance -= donationAmount;
+            currentAmountSpan.innerText = parseFloat(currentAmountSpan.innerText) + donationAmount;
+            document.getElementById('accountBalance').innerText = accountBalance;
+            addToHistory(card.querySelector('h3').innerText, donationAmount);
+            showModal();
+            inputField.value = '';
+        }
+    };
+});
+document.getElementById('closeModal').onclick = function() {
+    document.getElementById('modal').classList.add('hidden');
+};
+
+document.getElementById('blogButton').onclick = function() {
+    window.location.href = 'blog.html';
+};
